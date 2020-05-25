572323f6be7d1e5657795e0483ca52aff549988d
wangyin77 <wangyin1516@mails.jlu.edu.cn>
2020-05-25 15:01:51 +0800

    200525-链表练习2

---
+++ 链表/142.环形链表-ii.cpp
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=cpp
+ *
+ * [142] 环形链表 II
+ *
+ * https://leetcode-cn.com/problems/linked-list-cycle-ii/description/
+ *
+ * algorithms
+ * Medium (50.29%)
+ * Likes:    470
+ * Dislikes: 0
+ * Total Accepted:    78K
+ * Total Submissions: 155K
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+ * 
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+ * 
+ * 说明：不允许修改给定的链表。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：tail connects to node index 1
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ * 
+ * 
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：head = [1,2], pos = 0
+ * 输出：tail connects to node index 0
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ * 
+ * 
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入：head = [1], pos = -1
+ * 输出：no cycle
+ * 解释：链表中没有环。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：
+ * 你是否可以不用额外空间解决此题？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(!head) return NULL;
+        ListNode *fast = head, *low = head;
+        do{
+            fast = fast->next;
+            if(!fast) return NULL;
+            fast = fast->next;
+            if(!fast) return NULL;
+            low = low->next;
+        }while(fast != low);//用do..while可以少写点判断
+        low = head;
+        while(fast != low){
+            fast = fast->next;
+            low = low->next;
+        }
+        return fast;
+    }
+};
+// @lc code=end
+
---
+++ 链表/147.对链表进行插入排序.cpp
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=147 lang=cpp
+ *
+ * [147] 对链表进行插入排序
+ *
+ * https://leetcode-cn.com/problems/insertion-sort-list/description/
+ *
+ * algorithms
+ * Medium (64.35%)
+ * Likes:    161
+ * Dislikes: 0
+ * Total Accepted:    29.7K
+ * Total Submissions: 46.1K
+ * Testcase Example:  '[4,2,1,3]'
+ *
+ * 对链表进行插入排序。
+ * 
+ * 
+ * 插入排序的动画演示如上。从第一个元素开始，该链表可以被认为已经部分排序（用黑色表示）。
+ * 每次迭代时，从输入数据中移除一个元素（用红色表示），并原地将其插入到已排好序的链表中。
+ * 
+ * 
+ * 
+ * 插入排序算法：
+ * 
+ * 
+ * 插入排序是迭代的，每次只移动一个元素，直到所有元素可以形成一个有序的输出列表。
+ * 每次迭代中，插入排序只从输入数据中移除一个待排序的元素，找到它在序列中适当的位置，并将其插入。
+ * 重复直到所有输入数据插入完为止。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入: 4->2->1->3
+ * 输出: 1->2->3->4
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入: -1->5->3->4->0
+ * 输出: -1->0->3->4->5
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {// 这题比较难的
+        ListNode *dummy = new ListNode(-1);
+        while(head){//用head循环可以少定义一个指针
+            ListNode *next = head->next, *t = dummy;
+            while(t->next && t->next->val <= head->val) t = t->next;
+            //一定要循环到结尾，不能只到head
+            head->next = t->next, t->next = head;
+            //head总有地方放，head是最大的时候，t到了链表结尾，head指空
+            head = next;
+        }
+        return dummy->next;        
+    }
+};
+// @lc code=end
+
